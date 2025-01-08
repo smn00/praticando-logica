f3506dd18b1071bd1895e32a394b976e671b24f6
Noemia Moitinho <noemiamoitinho@gmail.com>
2025-01-08 20:10:50 -0300

    atuazacao do soteador-numero e alugames

---
+++ alugames/js/app.js
@@ -0,0 +1,20 @@
+function alterarStatus(id){
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let botao = jogoClicado.querySelector('.dashboard__item__button'); 
+    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
+
+    if(imagem.classList.contains('dashboard__item__img--rented')){
+            if(confirm(`Você tem certeza que deseja devolver esse Jogo ${nomeJogo.textContent}?`)){
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return')
+            botao.textContent = 'Alugar';
+        }
+
+    }else{
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return')
+        botao.textContent = 'Devolver';
+    }
+    
+}
